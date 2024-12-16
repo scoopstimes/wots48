54865e7f6a4c235cee73d34e08cc6186b8534b4f
Januar <adhinugroho623@gmail.com>
2024-12-16 22:32:37 +0700

    update

--- script/main.js
+++ script/main.js
@@ -7,3 +7,7 @@ function removeOverlayNavbar(event) {
   var overlayNavbar = event.currentTarget.querySelector('.overlay-navbar');
   overlayNavbar.classList.remove('active');
 }
+function goToWebsite(websiteURL) {
+      window.location.href = websiteURL;
+//alert("Server sedang diperbaiki, mohon menunggu.");
+    }
