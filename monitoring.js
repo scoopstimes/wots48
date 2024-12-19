e752a8e76b3df74a197b219a70c72fff1b04d847
Januar <adhinugroho623@gmail.com>
2024-12-19 16:58:52 +0700

    update monitoring.js

--- monitoring.js
+++ monitoring.js
@@ -2,31 +2,33 @@ document.addEventListener("DOMContentLoaded", function () {
   const sumber = 'https://api.crstlnz.my.id/api/now_live?group=jkt48';
 
   // Fungsi untuk mengirim notifikasi menggunakan OneSignal
-  function sendNotification(memberName, platform) {
+  function sendNotification(memberName, platform, liveTitle, memberImg) {
     const oneSignalUrl = "https://onesignal.com/api/v1/notifications";
     const apiKey = "os_v2_app_dxhckkbierahpoxitpgpvugliohijlkxgwauvdeb5y7l4akxayhbkme7v736to2tajc7itkis4ppw3zxsonafftkmds62m73i2dahsa"; // Ganti dengan API Key Anda
     const appId = "1dce2528-2824-4077-bae8-9bccfad0cb43";   // Ganti dengan App ID Anda
 
-    // Fungsi untuk memformat tipe platform
-    function formatPlatformText(platform) {
+    // Format teks platform dan isi notifikasi
+    function formatNotificationText(platform, liveTitle) {
       if (platform.toLowerCase() === 'idn') {
-        return 'IDN';
+        return `IDN Live: ${liveTitle}`;
       } else if (platform.toLowerCase() === 'showroom') {
-        return 'Showroom';
+        return `${memberName} sedang live di Showroom nih!`;
       }
-      return platform;
+      return `${memberName} sedang live di ${platform}!`;
     }
 
     const notificationData = {
       app_id: appId,
       headings: { en: "Live Notification" },
-      contents: { en: `${memberName} sedang live di ${formatPlatformText(platform)} nih!` }, // Format platform
+      contents: { en: formatNotificationText(platform, liveTitle) },
       included_segments: ["All"], // Kirim ke semua pengguna
       data: {
         member_name: memberName,
-        platform: formatPlatformText(platform), // Format data platform
+        platform,
+        live_title: liveTitle,
       },
-      url: `https://median.co/app/${memberName}`, // Opsional: URL ke aplikasi Median
+      big_picture: memberImg, // Gambar untuk notifikasi
+       
     };
 
     fetch(oneSignalUrl, {
@@ -53,20 +55,16 @@ document.addEventListener("DOMContentLoaded", function () {
         const liveMembers = data.filter((member) => member.started_at);
         const liveCount = liveMembers.length;
 
-        // Menampilkan jumlah member yang sedang live
         document.getElementById('liveCount').textContent = ` ${liveCount} Member`;
 
-        // Ambil indikator live
         const liveIndicator = document.querySelector('.live-indicator') || document.querySelector('.nolive-indicator');
 
         if (liveIndicator) {
           if (liveCount === 0) {
-            // Jika tidak ada yang live
             document.getElementById('noLiveMessage').textContent = 'Tidak ada yang live ';
             liveIndicator.classList.remove('live-indicator');
             liveIndicator.classList.add('nolive-indicator');
           } else {
-            // Jika ada yang live
             document.getElementById('noLiveMessage').textContent = '';
             liveIndicator.classList.remove('nolive-indicator');
             liveIndicator.classList.add('live-indicator');
@@ -74,26 +72,19 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         if (liveCount > 0) {
-          const idnUrl = 'https://www.idn.app/';
           liveMembers.slice(0, isLimit ? 100 : liveMembers.length).forEach((member) => {
-            console.log(member);
-
-            // Cek apakah member sudah mendapat notifikasi sebelumnya
             let notifiedMembers = JSON.parse(localStorage.getItem("notifiedMembers")) || [];
             let lastLiveTimestamp = localStorage.getItem("lastLiveTimestamp");
-
-            // Cek timestamp live member
             const memberLiveTime = new Date(member.started_at).getTime();
+
             const isNewLiveMember = memberLiveTime > lastLiveTimestamp;
 
-            // Jika member sebelumnya tidak live dan sekarang live, kirim notifikasi
-            if (!notifiedMembers.includes(member.name) || memberLiveTime > lastLiveTimestamp) {
-              sendNotification(member.name, member.type);
+            if (!notifiedMembers.includes(member.name) || isNewLiveMember) {
+              sendNotification(member.name, member.type, member.title || "Live", member.img);
 
-              // Simpan member yang sudah diberi notifikasi dan timestamp
               notifiedMembers.push(member.name);
-              localStorage.setItem("notifiedMembers", JSON.stringify(notifiedMembers)); // Simpan ke localStorage
-              localStorage.setItem("lastLiveTimestamp", memberLiveTime); // Simpan timestamp live terakhir
+              localStorage.setItem("notifiedMembers", JSON.stringify(notifiedMembers));
+              localStorage.setItem("lastLiveTimestamp", memberLiveTime);
             }
 
             const card = document.createElement('div');
@@ -145,48 +136,6 @@ document.addEventListener("DOMContentLoaded", function () {
               gap: 10px;
             `;
 
-            if (member.type === 'showroom') {
-              const showroomUrl = 'https://www.showroom-live.com/r/';
-              const showroomLink = document.createElement('a');
-              showroomLink.classList.add('btn-live-link', 'btn-primary');
-              showroomLink.innerHTML = '<span class="mdi mdi-arrow-top-right-thin-circle-outline"></span>';
-              showroomLink.href = showroomUrl + member.url_key;
-              showroomLink.target = '_blank';
-              cardBody.appendChild(showroomLink);
-
-              member.streaming_url_list.forEach((urlObj) => {
-                if (urlObj.label === 'original quality') {
-                  const fullscreenBtn = document.createElement('a');
-                  fullscreenBtn.classList.add('btn-live', 'btn-primary');
-                  fullscreenBtn.innerHTML = '<span class="mdi mdi-video"></span>';
-
-                  const startDate = member.started_at ? encodeURIComponent(member.started_at) : 'Tidak diketahui';
-                  const viewers = member.viewers ? encodeURIComponent(member.viewers) : '0';
-                  fullscreenBtn.href = `showroom.html#url=${encodeURIComponent(urlObj.url)}&name=${encodeURIComponent(member.name)}&viewers=${viewers}&start_date=${startDate}&type=${encodeURIComponent(member.type)}`;
-                  cardBody.appendChild(fullscreenBtn);
-                }
-              });
-            } else if (member.type === 'idn') {
-              const idnLink = document.createElement('a');
-              idnLink.classList.add('btn-live-link', 'btn-primary');
-              idnLink.innerHTML = '<span class="mdi mdi-arrow-top-right-thin-circle-outline"></span>';
-              idnLink.href = `${idnUrl}${member.url_key}/live/${member.slug}`;
-              cardBody.appendChild(idnLink);
-
-              member.streaming_url_list.forEach((urlObj) => {
-                const ProxyUrl = 'https://jkt48showroom-api.my.id/proxy?url=';
-                const fullscreenBtn = document.createElement('a');
-                fullscreenBtn.classList.add('btn-live', 'btn-primary');
-                fullscreenBtn.innerHTML = '<span class="mdi mdi-video"></span>';
-
-                const startDate = member.started_at ? encodeURIComponent(member.started_at) : 'Tidak diketahui';
-                const viewers = member.viewers ? encodeURIComponent(member.viewers) : '0';
-
-                fullscreenBtn.href = `idn.html#${ProxyUrl}${encodeURIComponent(urlObj.url)}&name=${encodeURIComponent(member.name)}&viewers=${viewers}&start_date=${startDate}&type=${encodeURIComponent(member.type)}`;
-                cardBody.appendChild(fullscreenBtn);
-              });
-            }
-
             card.appendChild(cardBody);
             container.prepend(card);
           });
@@ -195,15 +144,11 @@ document.addEventListener("DOMContentLoaded", function () {
       .catch((error) => console.error('Error fetching NOW LIVE:', error));
   }
 
-  // Fetch untuk SHOWROOM LIVE LIMIT
   fetchLiveData('.card-nowlive-container', true);
-
-  // Fetch untuk SHOWROOM LIVE NO LIMIT
   fetchLiveData('.card-nowlive-container-up', false);
 
-  // Set interval untuk otomatis refresh setiap 30 detik
   setInterval(() => {
     fetchLiveData('.card-nowlive-container', true);
     fetchLiveData('.card-nowlive-container-up', false);
-  }, 10000); // 30 detik
+  }, 10000); // 10 detik
 });
